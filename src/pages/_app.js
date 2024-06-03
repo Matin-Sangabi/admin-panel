84d5c2519d5dce773056f281b64a99772e1d5de3
unknown <sangabiwork@gmail.com>
2024-06-03 21:38:44 +0330

    chore : add react-query package

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,6 +8,9 @@ import Head from 'next/head'
 import { appText } from '../locales'
 import ThemeProvider from '../Context/ThemeContext'
 import LoadingProvider from '../Provider/LoadingProvider'
+import { HydrationBoundary, QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }) {
 	const getLayout = Component.getLayout ?? ((page) => page)
@@ -18,14 +21,18 @@ export default function App({ Component, pageProps }) {
 				<meta name='viewport' content='initial-scale=1, width=device-width' />
 				<title>{appText.title}</title>
 			</Head>
-			<ThemeProvider>
-				<NextUIProvider>
-					<LoadingProvider>
-						{getLayout(<Component {...pageProps} />)}
-						<ToastProvider />
-					</LoadingProvider>
-				</NextUIProvider>
-			</ThemeProvider>
+			<QueryClientProvider client={queryClient}>
+				<HydrationBoundary state={pageProps.dehydratedState}>
+					<ThemeProvider>
+						<NextUIProvider>
+							<LoadingProvider>
+								{getLayout(<Component {...pageProps} />)}
+								<ToastProvider />
+							</LoadingProvider>
+						</NextUIProvider>
+					</ThemeProvider>
+				</HydrationBoundary>
+			</QueryClientProvider>
 		</>
 	)
 }
